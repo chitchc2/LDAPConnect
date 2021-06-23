b6c0b181cce9d82b491739e03faddc483965dd34
Chad Hitchcock <chitchc2@gmail.com>
2021-06-23 18:21:02 -0400

    Add project files.

---
+++ LDAPConnect/LdapServer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.DirectoryServices.Protocols;
+using System.Globalization;
+using System.Net;
+using System.Security.Permissions;
+
+namespace LDAPConnect
+{
+    public class LdapServer
+    {
+
+        public LdapServer(string hostName, string baseDn, string filter)
+        {
+            this.HostName = hostName;
+            this.BaseDn = baseDn;
+            this.Filter = filter;
+        }
+
+        public string HostName { get; private set; }
+        public string BaseDn { get; set; }
+        public string Filter { get; set; }
+
+        [SecurityPermission(SecurityAction.Demand)]
+        public LdapUser Authenticate(string userName, string password)
+        {
+            using (var ldap = new LdapConnection(new LdapDirectoryIdentifier(this.HostName)))
+            {
+                ldap.SessionOptions.ProtocolVersion = 3;
+
+                ldap.AuthType = AuthType.Anonymous;
+                ldap.Bind();
+                var dn = GetDn(ldap, userName);
+
+                if (dn != null)
+                {
+                    try
+                    {
+                        ldap.AuthType = AuthType.Basic;
+                        ldap.Bind(new NetworkCredential(dn, password));
+                        return GetUser(ldap, dn);
+                    }
+                    catch (DirectoryOperationException ex1)
+                    { //Invalid user.
+                        Debug.WriteLine(ex1.Message);
+                    }
+                    catch (LdapException ex2)
+                    { //Invalid password.
+                        Debug.WriteLine(ex2.Message);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private String GetDn(LdapConnection ldap, String userName)
+        {
+            var request = new SearchRequest(this.BaseDn, string.Format(CultureInfo.InvariantCulture, this.Filter, userName), SearchScope.Subtree);
+            var response = (SearchResponse)ldap.SendRequest(request);
+            if (response.Entries.Count == 1)
+            {
+                return response.Entries[0].DistinguishedName;
+            }
+            return null;
+        }
+
+        private LdapUser GetUser(LdapConnection ldap, String dn)
+        {
+            var request = new SearchRequest(dn, "(objectclass=*)", SearchScope.Base);
+            var response = (SearchResponse)ldap.SendRequest(request);
+            if (response.Entries.Count == 1)
+            {
+                return new LdapUser(response.Entries[0]);
+            }
+            return null;
+        }
+
+    }
+}
---
+++ LDAPConnect/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
+using System.Net;
+using System.Text;
+
+namespace LDAPConnect {
+    class App
+    {
+        static void Main(string[] args)
+        {
+            var server = "192.168.56.100";
+            var baseDn = "dc=localdomain";
+            var filter = "uid={0}";
+
+            var userName = "jdoe";
+            var password = "test";
+
+            var ldap = new LdapServer(server, baseDn, filter);
+            var user = ldap.Authenticate(userName, password);
+            if (user != null)
+            {
+                Console.WriteLine("UID ......: " + user.Uid);
+                Console.WriteLine("Name .....: " + user.Name);
+                Console.WriteLine("First name: " + user.FirstName);
+                Console.WriteLine("Last name : " + user.LastName);
+            }
+            else
+            {
+                Console.WriteLine("Authorization failed.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
